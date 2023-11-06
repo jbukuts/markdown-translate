172d47bb95827499c09190b7645edf1ec0403ebc
Jake Bukuts <jbukuts@gmail.com>
2023-11-05 21:29:57 -0500

    rollup conf updated to account for workers in build binaries

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,29 @@
+/* eslint-disable no-useless-escape */
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'bin.js',
+  input: {
+    bin: 'bin.js',
+    'src/worker': 'src/worker.js'
+  },
   output: {
     dir: 'output',
     format: 'cjs'
   },
-  plugins: [commonjs(), json(), nodeResolve()]
+  plugins: [
+    commonjs(),
+    json(),
+    nodeResolve({
+      exportConditions: ['node'],
+      preferBuiltins: true
+    }),
+    replace({
+      "'./src/worker.js'": 'require.resolve("./src/worker.js")',
+      delimiters: ['', ''],
+      preventAssignment: true
+    })
+  ]
 };
